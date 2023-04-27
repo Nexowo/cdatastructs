e21f81ad638de4f1eb9d869aec490b6a7afc0ae0
Jean Maccou <49556588+Nexowo@users.noreply.github.com>
2023-04-27 21:38:23 +0200

    Delocalisation of main in main.c

--- plist.c
+++ plist.c
@@ -32,13 +32,3 @@ void* pop(list *l) {
     free(temp);
     return val;
 }
-
-int main(int argc, char *argv) {
-    list *a = create();
-    int x = 2;
-    int *px = &x;
-    a = append(a, &x);
-    px = (int*)pop(a);
-    printf("%d", x);
-    return 0;
-}

f8e9062d7d916cbfea72e4a72d971886918a8be4
nexowo <jmaccou@outlook.fr>
2023-04-29 15:17:12 +0200

    Doc

--- plist.h
+++ plist.h
@@ -5,15 +5,15 @@ typedef struct elem {
 	struct elem* next; /*Pointer on the next element of the list*/
 	struct elem* prev; /*Pointer on the previous element of the list*/
 	void* val; /*Generic pointer on the data*/
-}element;
+}element; /*This structure represents an element of a list*/
 
 typedef struct l {
 	element* head; /*Pointer on the head of the list*/
 	element* tail; /*Pointer on the tail of the list*/
 	int size; /*Size of the list*/
-} list;
+} list; /*double chained list with a pointer on head, on queue and an int representing the size*/
 
 list* create(); /*Creation of a new empty list*/
 list* append(list* l, void* val); /*Add a new element at the end of the list*/
-void* pop(list *l); /*Suppress the first element, free it and return the value*/
-void destroy(list *l); /*Free all the element of the list and the list itself*/
+void* pop(list* l); /*Suppress the first element, free it and return the value*/
+void destroy(list* l); /*Free all the element of the list and the list itself*/
